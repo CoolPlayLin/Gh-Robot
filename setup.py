3ef0e28af26959a73b655761125bb4914bc874dd
CoolPlayLin <CoolPlayLin@qq.com>
2022-09-23 20:41:26 +0800

    Update

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="Gh-Robot",
+    version="V0.0.1",
+    author="CoolPlayLin",
+    author_email="help@api-coolplaylin.eu.org",
+    description="A Github API-based Robot",
+    install_requires=[
+        'requests>=2.28.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU General Public License :: V3.0"
+    ]
+)
