46d146b55e7a5cbb2ae7b4ba7445f21245036a55
CoolPlayLin <CoolPlayLin@qq.com>
2022-09-24 09:41:38 +0800

    Update

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from setuptools import find_packages
 import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="Gh-Robots",
@@ -9,6 +11,7 @@ setuptools.setup(
     description="A Github API-based Robot",
     long_description=long_description,
     long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
     license='GPL V3.0',
     project_urls={
         "Bug Report": "https://github.com/CoolPlayLin/Gh-Robot/issues/new/choose"
