ecbf72e45edf9dff8a4e6191abd8ad0491148c84
CoolPlayLin <CoolPlayLin@qq.com>
2022-10-29 13:49:34 +0800

    Update Some Modo & Add New

--- GhRobot/Bot.py
+++ GhRobot/Bot.py
@@ -3,63 +3,62 @@ import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 # 获取所有Latest-Release链接
-def GetAllLatestDownloadURL(User, Repo, Format=None):
+def GetAllLatestDownloadURL(User, Repo, Select=None, TXT=False):
   requests.packages.urllib3.disable_warnings(InsecureRequestWarning) #禁用SSL证书验证警告
   UrlList = []
-  if bool(Format) is False:
-    ApiURL="https://api.github.com/repos/{}/{}/releases/latest".format(User, Repo)
-    IntJson = requests.get(url=ApiURL, verify=False).text
-    LoadJson = json.loads(IntJson, strict=False)
-    try:
-      if LoadJson["message"] == "Not Found":
-        return(1)
-    except:
-      pass
-    try:
+  ApiURL="https://api.github.com/repos/{}/{}/releases/latest".format(User, Repo)
+  IntJson = requests.get(url=ApiURL, verify=False).text
+  LoadJson = json.loads(IntJson, strict=False)
+  if bool(TXT) is False:
+    if bool(Select) is False:
+      try:
+          x = 0
+          while True:
+              UrlList.append(LoadJson["assets"][x]["browser_download_url"])
+              x += 1
+      except (IndexError, KeyError):
+        return(UrlList)
+    else:
+      try:
         x = 0
         while True:
+          Cache = LoadJson["assets"][x]["browser_download_url"]
+          if Select in Cache:
             UrlList.append(LoadJson["assets"][x]["browser_download_url"])
-            x += 1
-    except IndexError:
-      return(UrlList)
-    except Exception as WhyError:
-      print(WhyError)
-      return(1)
+          x += 1
+      except (IndexError, KeyError):
+        return(UrlList)
   else:
-    ApiURL="https://api.github.com/repos/{}/{}/releases/latest".format(User, Repo)
-    IntJson = requests.get(url=ApiURL, verify=False).text
-    LoadJson = json.loads(IntJson, strict=False)
-    try:
-      if LoadJson["message"] == "Not Found":
-        return(1)
-    except:
-      pass
-    try:
-      x = 0
-      while True:
-        Cache = LoadJson["assets"][x]["browser_download_url"]
-        if Format in Cache:
-          UrlList.append(LoadJson["assets"][x]["browser_download_url"])
-        else:
-          pass
-        x += 1
-    except IndexError:
-      return(UrlList)
-    except Exception as WhyError:
-      print(WhyError)
-      return(1)
+    if type(TXT) == bool:
+      TheFileNameWrittenToTXT = "Urls.txt"
+    else:
+      TheFileNameWrittenToTXT = str(TXT)
+    if bool(Select) is False:
+      with open(TheFileNameWrittenToTXT, "w+", newline="", encoding="utf-8") as file:
+        try:
+          x = 0
+          while True:
+            file.write(LoadJson["assets"][x]["browser_download_url"]+"\n")
+            x += 1
+        except (IndexError, KeyError):
+          return(0)
+    else:
+      with open(TheFileNameWrittenToTXT, "w+", newline="", encoding="utf-8") as file:
+        try:
+          x = 0
+          while True:
+            if Select in LoadJson["assets"][x]["browser_download_url"]:
+              file.write(LoadJson["assets"][x]["browser_download_url"]+"\n")
+            x += 1
+        except (IndexError, KeyError):
+          return(0)
 
 # 获取指定Latest-Release链接
 def GetLatestDownloadURL(User, Repo, Order):
   requests.packages.urllib3.disable_warnings(InsecureRequestWarning) #禁用SSL证书验证警告
-  ApiURL="https://api.github.com/repos/"+User+"/"+Repo+"/releases/latest"
+  ApiURL="https://api.github.com/repos/{}/{}/releases/latest".format(User, Repo)
   IntJson = requests.get(url=ApiURL, verify=False).text
   LoadJson = json.loads(IntJson, strict=False)
-  try:
-    if LoadJson["message"] == "Not Found":
-      return(1)
-  except:
-    pass
   try:
     return(LoadJson["assets"][Order]["browser_download_url"])
   except Exception as WhyError:
