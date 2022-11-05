5fe77769afbda9c10d29b112528f15cd70725e7d
CoolPlayLin <CoolPlayLin@qq.com>
2022-11-05 14:39:15 +0800

    Update

--- setup.py
+++ setup.py
@@ -1,43 +1,63 @@
 import setuptools
+import os
 
-# 读取README文件
-while True:
-    try:
-        with open("README.md", "r", encoding="utf-8") as fh:
-            long_description = fh.read()
-        break
-    except:
-        with open(r"./docs/README.md", "r", encoding="utf-8") as fh:
-            with open("README.md", "w+", encoding="utf-8") as writer:
-                writer.write(fh.read())
+# 运行必须变量
+RUN = True
+EndTimes = 0
+READMEFile = "README.md"
 
-# 设置主体
-setuptools.setup(
-    name="Gh-Robots",
-    version="0.0.5",
-    author="CoolPlayLin",
-    author_email="help@api-coolplaylin.eu.org",
-    description="A Github API-based Robot",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(),
-    license='GPL',
-    project_urls={
-        "Bug Report": "https://github.com/CoolPlayLin/Gh-Robot/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBug%5D%3A",
-        "Feature Request": "https://github.com/CoolPlayLin/Gh-Robot/issues/new?assignees=&labels=enhancement&template=feature-request.md&title=%5BFeature+Request%5D"
-    },
-    download_url="https://github.com/CoolPlayLin/Gh-Robot/releases",
-    url="https://github.com/CoolPlayLin/Gh-Robot",
-    install_requires=[
-        'requests>=2.28.1',
-        'wget>=3.2',
-        'pandas>=1.5.0'
-    ],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
-    ]
-)
+def main():
+    global EndTimes
+    global RUN
+    global READMEFile
+
+    while RUN:
+        if EndTimes > 5:
+            try:
+                os.remove(READMEFile)
+            except:
+                pass
+            return 1
+        try:
+            with open(READMEFile, "r", encoding="utf-8") as fh:
+                long_description = fh.read()
+            RUN = False
+        except:
+            with open(r"./docs/"+READMEFile, "r", encoding="utf-8") as fh:
+                with open(READMEFile, "w+", encoding="utf-8") as writer:
+                    writer.write(fh.read())
+        EndTimes += 1
+
+    setuptools.setup(
+        name="Gh-Robots",
+        version="0.0.5",
+        author="CoolPlayLin",
+        author_email="help@api-coolplaylin.eu.org",
+        description="A Github API-based Robot",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=setuptools.find_packages(),
+        license='GPL',
+        project_urls={
+            "Bug Report": "https://github.com/CoolPlayLin/Gh-Robot/issues/new?assignees=&labels=bug&template=bug_report.md&title=%5BBug%5D%3A",
+            "Feature Request": "https://github.com/CoolPlayLin/Gh-Robot/issues/new?assignees=&labels=enhancement&template=feature-request.md&title=%5BFeature+Request%5D"
+        },
+        download_url="https://github.com/CoolPlayLin/Gh-Robot/releases",
+        url="https://github.com/CoolPlayLin/Gh-Robot",
+        install_requires=[
+            'requests>=2.28.1',
+            'wget>=3.2',
+            'pandas>=1.5.0'
+        ],
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Natural Language :: English",
+            "Programming Language :: Python :: 3",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+        ]
+    )
+    return 0
+
+if __name__ == "__main__":
+    main()
